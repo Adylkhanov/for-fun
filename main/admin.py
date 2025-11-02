853753896bb7c12d5f5a1a92c15183c719dc03e5
Alisher <>
2025-11-02 16:27:04 +0500

    Initial commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Category, Size, Product, \
+    ProductImage, ProductSize
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+
+
+class ProductAdmin(admin.TabularInline):
+    list_display = ['name', 'price', 'category', 'color']
+    list_filter = ['category', 'color']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines =  [ProductImageInline, ProductSizeInline]
+
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Size, SizeAdmin)
+admin.site.register(Product, ProductAdmin)
+
+
